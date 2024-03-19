deea8c4c148b0894ec474c32ad69e55040651cea
Anastasia Grigoryeva <87610885+staciess@users.noreply.github.com>
2024-03-19 22:43:45 +0200

    Update Either.hs

--- monads/src/Either.hs
+++ monads/src/Either.hs
@@ -5,11 +5,44 @@ data MyEither a b
   | MyRight b 
   deriving (Show, Eq)
 
--- Implement the instance and prove the laws
-instance Functor (MyEither a)
+instance Functor (MyEither a) where
+  fmap _ (MyLeft a) = MyLeft a
+  fmap f (MyRight b) = MyRight (f b)
 
--- Implement the instance and prove the laws
-instance Applicative (MyEither a) 
+{-
+fmap id (MyLeft a) = MyLeft a
+fmap id (MyRight b) = MyRight (id b) = MyRight b
 
--- Implement the instance and prove the laws
-instance Monad (MyEither a)
+fmap (f . g) (MyLeft a) = MyLeft a = fmap f (MyLeft a) = fmap f (fmap g (MyLeft a)) = fmap f . fmap g (MyLeft a)
+fmap (f . g) (MyRight b) = MyRight (f (g b)) = fmap f (MyRight (g b)) = fmap f (fmap g (MyRight b)) = fmap f . fmap g (MyRight b)
+-}
+
+instance Applicative (MyEither a) where
+  pure = MyRight
+  (MyLeft a) <*> _ = MyLeft a
+  (MyRight b) <*> r = fmap b r
+
+{-
+pure id <*> (MyLeft a) = MyLeft a
+pure id <*> (MyRight b) = MyRight (id b) = MyRight b
+
+pure f <*> pure r = MyRight (f r)
+
+MyLeft a <*> pure y = MyLeft a = pure ($ y) <*> MyLeft a
+MyRight f <*> pure y = MyRight (f y) = pure ($ y) <*> MyRight f
+
+pure (.) <*> u <*> v <*> w = u <*> (v <*> w)
+-}
+  
+instance Monad (MyEither a) where
+  return = pure
+  (MyRight x) >>= f = f x
+  (MyLeft a) >>= _ = MyLeft a
+  
+{-
+return a >>= f = MyRight a >>= f = f a  --lft
+MyRight x >>= return = return x = MyRight x 
+MyLeft a >>= return = MyLeft a  --right
+
+(m >>= f) >>= g = (MyEither >>= f) >>= g = m >>= (\x -> f x >>= g)
+-}
